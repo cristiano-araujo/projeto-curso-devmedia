2dc8781439dd1c879b10c92aff2bda517ae13aba
cristiano-araujo <cristiano112715@gmail.com>
2023-12-30 14:27:00 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/componentes/SecaoCapa/index.js
@@ -0,0 +1,13 @@
+import './estilos.css';
+
+export default function SecaoCapa() {
+    return(
+        <section className='secao-capa'>
+            <div className='limitar-secao'>
+                <p>Preços Baixos em</p>
+                <h1>Oculos de grau e de sol</h1>
+                <p>Você só encontra aqui</p>
+            </div>
+       </section>
+    );
+}
